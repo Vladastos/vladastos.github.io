d2b7078e122b268cdc0fea5a055b439facf426cf
vladastos <vladislavparfeniuc@gmail.com>
2024-02-16 17:45:41 +0100

    Added FontAwesome icons, extrapolated sidebar into separate component

--- vladastos/src/app/app.module.ts
+++ vladastos/src/app/app.module.ts
@@ -1,13 +1,24 @@
+import {
+  FontAwesomeModule,
+  FaIconLibrary,
+} from '@fortawesome/angular-fontawesome';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { faBars, faClose } from '@fortawesome/free-solid-svg-icons';
+import { SidebarModule } from './sidebar/sidebar.module';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule, SidebarModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faBars, faClose);
+  }
+}
