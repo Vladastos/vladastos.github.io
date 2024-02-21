23c496164ad6b9819dc18c0e10264321c804d47e
vladastos <vladislavparfeniuc@gmail.com>
2024-02-21 14:45:39 +0100

    added placeholder theme

--- vladastos/tailwind.config.js
+++ vladastos/tailwind.config.js
@@ -5,4 +5,30 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#e11d48",
+
+          secondary: "#00f7bb",
+
+          accent: "#0069db",
+
+          neutral: "#44403c",
+
+          "base-100": "#fcfcfc",
+
+          info: "#0062f4",
+
+          success: "#009b35",
+
+          warning: "#facc15",
+
+          error: "#ed003e",
+        },
+      },
+      "dark",
+    ],
+  },
 };
